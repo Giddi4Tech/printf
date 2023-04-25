d688c1b0c71e737b64b81d5ebbd51c4f2f426c33
TheKharleeci <falodunmodupeola@gmail.com>
2023-04-25 23:49:32 +0300

    update main.h

--- main.h
+++ main.h
@@ -113,5 +113,6 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+buffer_t *init_buffer(void);
 
 #endif /* MAIN_H */
